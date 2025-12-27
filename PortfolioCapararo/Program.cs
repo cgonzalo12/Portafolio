ea8cb0669dfdd82311404b0382e1b15d96aa7e8d
cgonzalo12 <c.gonzalo12@gmail.com>
2025-12-27 19:35:25 -0300

    terminadocompleto

--- PortfolioCapararo/Program.cs
+++ PortfolioCapararo/Program.cs
@@ -122,7 +122,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000")
+        policy.WithOrigins("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000", "https://portafolio-front-end-eight.vercel.app")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
